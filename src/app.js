97f862a963254db89fc896471fed43f556512ad5
j-dawg22 <jahangir2222@gmail.com>
2018-07-28 20:56:45 +0600

    trimmed app.js

--- src/app.js
+++ src/app.js
@@ -12,33 +12,6 @@ import "./styles/styles.scss";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    description: "Water bill",
-    amount: 4500,
-    createdAt: 0
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Gas bill",
-    createdAt: 91050200,
-    amount: 7200
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Rent",
-    amount: 109500,
-    createdAt: -91050200
-  })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
